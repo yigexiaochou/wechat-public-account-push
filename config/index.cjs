50c89233414af1a1c3c20dcf28312e62208b9c54
yigexiaochou <128983225+yigexiaochou@users.noreply.github.com>
2025-05-13 15:35:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oB3zY7YpvLbgk8-F1ABrbtkMc2Zo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'fyo3LQyikCu1UeIa5EMbAzOu8zRJsaGlkSgx9ZnzZ9o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
