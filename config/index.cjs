95ef63c7e8b0580c43777c49a6f415246f912f83
yigexiaochou <128983225+yigexiaochou@users.noreply.github.com>
2025-05-13 16:14:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'j_TYucqJ7Xk322snLw0JqGagCRwnGSP-mA0vAO6whu0',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
